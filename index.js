d09d7c423513e0f2efaa701e0fbe30d0b4b15123
Kevin Kekisev <Kevkekisev@gmail.com>
2022-08-24 18:13:42 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,62 @@
+const form = document.getElementById("generate-form");
+const qr = document.getElementById("qrcode");
+
+function onCodeGenerateSubmit(e) {
+  e.preventDefault();
+
+  clearQr();
+
+  const url = document.getElementById("url").value;
+  const size = document.getElementById("size").value;
+
+  if (url === "") {
+    alert("Please enter a URL");
+  } else {
+    showSpinner();
+
+    setTimeout(() => {
+      hideSpinner();
+      generateQR(url, size);
+      setTimeout(() => {
+        const saveUrl = qr.querySelector("img").src;
+        createSaveBtn(saveUrl);
+      }, 50);
+    }, 1000);
+  }
+}
+
+function generateQR(url, size) {
+  const qrCode = new QRCode("qrcode", {
+    text: url,
+    width: size,
+    height: size,
+  });
+}
+
+function showSpinner() {
+  document.getElementById("spinner").style.display = "block";
+}
+function hideSpinner() {
+  document.getElementById("spinner").style.display = "none";
+}
+
+function clearQr() {
+  qr.innerHTML = "";
+  const saveLink = document.getElementById("sav -link");
+  if (saveLink) saveLink.remove();
+}
+
+function createSaveBtn(saveUrl) {
+  const link = document.createElement("a");
+  link.id = "save-link";
+  link.classList =
+    "bg-blue-500 hover:bg-gray-700 text-white font-bold py-2 rounded w-1/3 m-auto my-5";
+  link.href = saveUrl;
+  link.download = "qrcode";
+  link.innerHTML = "Save Image";
+  document.getElementById("generated").appendChild(link);
+}
+
+hideSpinner();
+
+form.addEventListener("submit", onCodeGenerateSubmit);
